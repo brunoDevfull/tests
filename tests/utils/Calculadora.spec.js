a0774ea80bde73755b3669d4eb7c8059d0d544bf
Bruno <brunoribeiro.cgms@gmail.com>
2025-09-24 21:36:47 -0400

    metodo dividir

--- tests/utils/Calculadora.spec.js
+++ tests/utils/Calculadora.spec.js
@@ -190,7 +190,65 @@ describe("Testes unitarios da classe calculadora", () => {
 
 
   });
+
+  describe("Teste do método dividir", () => {
+
+    test("deve dividir dois numeros positivos", () => {
+      //ACT
+      const resultado = calculadora.dividir(10, 10);
+
+      //ASSERT
+      expect(resultado).toBe(1);
+    });
+
+    test("deve dividir dois numeros negativos", () => {
+      //ACT
+      const resultado = calculadora.dividir(-10, -10);
+
+      //ASSERT
+      expect(resultado).toBe(1);
+    });
+
+    test("Deve dividir um número positivo e um número negativo.", () => {
+      //ACT
+      const resultado = calculadora.dividir(10, -10);
+
+      //ASSERT
+      expect(resultado).toBe(-1);
+    });
+
+    test("Deve dividir um número negativo e um número positivo.", () => {
+      //ACT
+      const resultado = calculadora.dividir(-10, 10);
+
+      //ASSERT
+      expect(resultado).toBe(-1);
+    });
+
+    test("Deve lançar um erro se o primeiro parâmetro não for um número.", () => {
+      //ASSERT
+      expect(() => {
+        //ACT
+        calculadora.dividir("a", 5);
+      }).toThrow(TypeError);
+    });
+
+    test("Deve lançar um erro se o segundo parâmetro não for um número.", () => {
+      //ASSERT
+      expect(() => {
+        //ACT
+        calculadora.dividir(5, "b");
+      }).toThrow(TypeError);
+    });
+
+    test("Deve lançar um erro se ambos os parâmetros não forem números.", () => {
+      //ASSERT
+      expect(() => {
+        //ACT
+        calculadora.dividir("a", "b");
+      }).toThrow(TypeError);
+    });
+
+  });
     
-
-
 });
